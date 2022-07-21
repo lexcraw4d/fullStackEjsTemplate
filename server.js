c2dda5f54c3c2e5a27f00e49a7c2df0a2c1252a6
Lex Crawford <lexcraw4d@gmail.com>
2022-07-20 23:11:49 -0400

    boiler plate ejs fs template with mongodb

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const PORT = 8500
+const TestModel = require('./models/schema.sql')
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.DB_STRING, 
+            {useNewUrlParser:true})
+            console.log(`connected to the database', ${mongoose.connection.name}`)
+    } catch (err) {
+        console.log('Failed to connect', err)
+    }
+}
+connectDB()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(cors())
+
+app.get('/', async (request, response) => {
+    try {
+        //Get data from DB
+        const content = await TestModel.find()
+        console.log(content)
+        response.render('index.ejs', {contentKey:content})
+        //After data is found, then render ejs AND pass the data so that it can rendor on the page
+    } catch (error) {
+        response.status(500).send({message: error.message})
+    }
+})
+
+
+//PORT = 8000
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
